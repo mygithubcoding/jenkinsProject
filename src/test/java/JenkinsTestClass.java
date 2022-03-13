df994fa9ff2749d8fd66533887b27ae93c1f1fab
mygithubcoding <mysignupemail16@gmail.com>
2022-03-12 19:23:35 -0500

    This is first commit for Jenkins from GITHUB

---
+++ src/test/java/JenkinsTestClass.java
@@ -0,0 +1,24 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author deept
+ *
+ */
+public class JenkinsTestClass {
+	@Test
+	public void m1() {
+		System.out.println("Jenkins first method");
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test
+	public void m2() {
+		System.out.println("Jenkins second method");
+		Assert.assertEquals(true, false);
+	}
+}
